06513539059631e9476f5f3e737bc6efe7bb8aff
Baturax <bai@baturax.com>
2025-10-08 20:04:37 +0000

    Update player controls and styles with SVG icons; enhance layout and responsiveness

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,13 @@ import { createSignal, onMount, Show, type Component } from "solid-js";
 
 import styles from "./App.module.css";
 
+import PlayButton from "./assets/play.svg";
+import PauseButton from "./assets/pause.svg";
+import PreviousButton from "./assets/previous.svg";
+import NextButton from "./assets/next.svg";
+import Prev5Button from "./assets/replay5.svg";
+import Next5Button from "./assets/forward5.svg";
+
 const App: Component = () => {
   const [playerInfo, setPlayerInfo] = createSignal({
     playername: "",
@@ -32,35 +39,51 @@ const App: Component = () => {
 
   setInterval(() => {
     bai();
-  }, 1000);
+  }, 500);
 
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <button id="play-pause-btn" onClick={PlayPause}>
-          Play/Pause
-        </button>
+        <Show when={playerInfo().status == "Playing"}>
+          <button id="play-pause-btn" onClick={PlayPause}>
+            <img src={PauseButton}/>
+          </button>
+        </Show>
+
+        <Show when={playerInfo().status == "Paused"}>
+          <button id="play-pause-btn" onClick={PlayPause}>
+            <img src={PlayButton}/>
+          </button>
+        </Show>
 
         <div id="media-box">
-          <h1>{playerInfo().title}</h1>
+          <h1 id="title-h3">{playerInfo().title}</h1>
           <div id="extra-media">
             <Show when={playerInfo().artist}>
-              <h3>Artist: {playerInfo().artist}</h3>
+              <h3 id="artist-h3">Artist: {playerInfo().artist}</h3>
             </Show>
             <Show when={playerInfo().album}>
-              <h3>Album: {playerInfo().album}</h3>
+              <h3 id="album-h3">Album: {playerInfo().album}</h3>
             </Show>
           </div>
         </div>
 
-        <div id="controller">
-          <button onClick={Previous}>Previous</button>
-          <button onClick={BackFive}>BackFive</button>
-          <p>{playerInfo().position}</p>
-          <p>----------------------</p>
-          <p>{playerInfo().length}</p>
-          <button onClick={Next}>Next</button>
-          <button onClick={NextFive}>NextFive</button>
+        <div class={styles.controller}>
+          <div class={styles["controller-left"]}>
+            <button onClick={Previous}><img src={PreviousButton}/></button>
+            <button onClick={BackFive}><img src={Prev5Button}/></button>
+          </div>
+
+          <div class={styles["controller-center"]}>
+            <p class={styles.position}>{playerInfo().position}</p>
+            <div class={styles.progressBar}></div>
+            <p class={styles.length}>{playerInfo().length}</p>
+          </div>
+
+          <div class={styles["controller-right"]}>
+            <button onClick={NextFive}><img src={Next5Button}/></button>
+            <button onClick={Next}><img src={NextButton}/></button>
+          </div>
         </div>
       </header>
     </div>
