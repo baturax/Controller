80ee47e39d9a2b9bf097493a7acf9900745f4167
Baturax <bai@baturax.com>
2025-10-08 18:45:40 +0000

    Refactor App component to fetch and display player metadata, including title, artist, album, and length

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,67 @@
-import type { Component } from "solid-js";
+import { createSignal, onMount, Show, type Component } from "solid-js";
 
-import logo from "./logo.svg";
 import styles from "./App.module.css";
 
 const App: Component = () => {
+  const [playerInfo, setPlayerInfo] = createSignal({
+    playername: "",
+    position: "",
+    status: "",
+    volume: "",
+    album: "",
+    artist: "",
+    title: "",
+    length: "",
+  });
+
+  function bai() {
+    fetch("http://192.168.1.161:31313/api/info")
+      .then((response) => {
+        if (!response.ok) throw new Error("HTTP error " + response.status);
+        return response.json();
+      })
+      .then((data) => {
+        setPlayerInfo(data);
+      })
+      .catch((err) => console.error("Fetch error:", err));
+  }
+
+  onMount(() => {
+    bai();
+  });
+
+  setInterval(() => {
+    bai();
+  }, 1000);
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <button onClick={Previous}>Previous</button>
-        <button onClick={PlayPause}>Play/Pause</button>
-        <button onClick={Next}>Next</button>
-        <br />
-        <button onClick={BackFive}>BackFive</button>
-        <h1>Song Info Placeholder</h1>
-        <button onClick={NextFive}>NextFive</button>
+        <button id="play-pause-btn" onClick={PlayPause}>
+          Play/Pause
+        </button>
+
+        <div id="media-box">
+          <h1>{playerInfo().title}</h1>
+          <div id="extra-media">
+            <Show when={playerInfo().artist}>
+              <h3>Artist: {playerInfo().artist}</h3>
+            </Show>
+            <Show when={playerInfo().album}>
+              <h3>Album: {playerInfo().album}</h3>
+            </Show>
+          </div>
+        </div>
+
+        <div id="controller">
+          <button onClick={Previous}>Previous</button>
+          <button onClick={BackFive}>BackFive</button>
+          <p>{playerInfo().position}</p>
+          <p>----------------------</p>
+          <p>{playerInfo().length}</p>
+          <button onClick={Next}>Next</button>
+          <button onClick={NextFive}>NextFive</button>
+        </div>
       </header>
     </div>
   );
