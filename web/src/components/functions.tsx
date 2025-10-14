fbe711de09a9f094b1e3367edc2c034cfc7c9454
Baturax <bai@baturax.com>
2025-10-14 12:28:52 +0300

    fix: sync client control fetch URLs with renamed API routes (rewind-5-sec, forward-5-sec, volume-up-5, volume-down-5)

--- web/src/components/functions.tsx
+++ web/src/components/functions.tsx
@@ -17,25 +17,25 @@ export function Next() {
 }
 
 export function BackFive() {
-  fetch("/api/fiveminus").then(function () {
+  fetch("/api/rewind-5-sec").then(function () {
     console.log("5 Seconds Minus K");
   });
 }
 
 export function NextFive() {
-  fetch("/api/fiveplus").then(function () {
+  fetch("/api/forward-5-sec").then(function () {
     console.log("5 Seconds Plus K");
   });
 }
 
 export function IncVolume() {
-  fetch("/api/uppervolume").then(function(){
+  fetch("/api/volume-up-5").then(function(){
     console.log("+5 volume okay");
   })
 }
 
 export function DecVolume() {
-  fetch("/api/lowervolume").then(function(){
+  fetch("/api/volume-down-5").then(function(){
     console.log("-5 volume okay");
   })
 }
